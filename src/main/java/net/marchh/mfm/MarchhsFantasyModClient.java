07e9fa38aeb40428639c412fcfec52f54bbc1a00
E N <nault.elijah@gmail.com>
2024-04-10 14:48:32 -0700

    first commit

---
+++ src/main/java/net/marchh/mfm/MarchhsFantasyModClient.java
@@ -0,0 +1,10 @@
+package net.marchh.mfm;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MarchhsFantasyModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
